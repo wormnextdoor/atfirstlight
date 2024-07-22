cdc5dcd43a10e6e2ec75d822ecabfe283ae62faf
wormnextdoor <kay.lee45@outlook.com>
2024-07-22 13:48:02 -0600

    add files - script test

---
+++ script.js
@@ -0,0 +1,90 @@
+function sendToPage(){
+    var input = document.getElementById("search").value;
+     //alert(input);
+    if (input == "explain"){
+        location.href = "rcf-lorelei.html";
+        return false;
+    }
+    else if (input == "reemerge"){
+        location.href = "rcf-2024.html";
+        return false;
+    }
+    else if (input == "5146974399"){
+        location.href = "rcf-adelaide1";
+        return false;
+    }
+    else if (input == "4382867144"){
+        location.href = "rcf-2029.html";
+        return false;
+    }
+    else if (input == "placeholder key"){
+        location.href = "rcf-2032.html";
+        return false;
+    }
+    else if (input == "river"){
+        alert("she defended you to all her friends, you know. just for you to prove them right.");
+    }
+    else if (input == "castel"){
+        alert("don't.");
+    }
+    else if (input == "milo"){
+        alert("you really think your own name will unlock something special? please. don't flatter yourself.");
+    }
+    else if (input == "max"){
+        alert("he's dead. you killed him. or have you forgotten?");
+    }
+    else if (input == "maximilliano"){
+        alert("what would your parents say if they knew what you've done?");
+    }
+    else if (input == "florian"){
+        alert("i'm still here.");
+    }
+    else if (input == "maia"){
+        alert("she spent hours waiting for river to come home. just thought you should know that.");
+    }
+    else if (input == "rowan"){
+        alert("what happened to him was your fault, too. never forget that.");
+    }
+    else if (input == "adelaide"){
+        alert("do you wonder what could've been?");
+    }
+    else if (input == "quinn"){
+        alert('she thought you were good. can you believe it? she thought that by helping you, she was doing something GOOD.');
+    }
+    else if (input == "lola"){
+        alert('are you proud of how you treated her?');
+    }
+    else if (input == "aurorae"){
+        alert('8');
+    }
+    else if (input == "soren"){
+        alert('i forgive her.');
+    }
+    else if (input == "vi"){
+        alert("i'm glad she's doing better now.");
+    }
+    else if (input == "aviana"){
+        alert("don't you know it's rude to look through people's memories?");
+    }
+    else if (input == "remember"){
+        alert("jlq xtpyxyx ksg dslch eehf lvi");
+    }
+    else if (input == "zinnia"){
+        alert("i won't apologize. not to you.");
+    }
+    else if (input == "help"){
+        alert('you cannot be saved.');
+    }
+    else if (input == "at first light"){
+        alert('dawn is approaching.');
+    }
+    else if (input == "the eye of the beholder"){
+        alert('how did it end?');
+    }
+    else if (input == "coffee at midnight"){
+        alert('you can hear it in the silence.');
+    }
+    else {
+        alert('invalid input.');
+    }
+        }
