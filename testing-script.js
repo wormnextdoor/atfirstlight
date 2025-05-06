95d2d4846c13a133f506964098c507ee44b32e3f
worm <kay.lee45@outlook.com>
2025-05-06 16:42:15 -0700

    create testing-script.js

---
+++ testing-script.js
@@ -0,0 +1,24 @@
+function sendToPage(){
+    var input = document.getElementById("search").value;
+     //alert("text");
+     //location.href = "file";
+    if (input == "zinnia"){
+        alert("can you hear me?");
+        return false;
+    }
+    else if (input == "remember"){
+        location.href = "message.html";
+        return false;
+    }
+        else if (input == "here"){
+        alert("ha ha, very funny.");
+        return false;
+    }
+        else if (input == "at first light"){
+        location.href = "afl.html";
+          return false;
+    }
+    else {
+        alert('invalid input.');
+    }
+        }
